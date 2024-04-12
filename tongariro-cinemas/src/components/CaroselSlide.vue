64154d97be2edbfd4bd1280e05ed3c540a0aae92
jvenske <20231508@mywhitecliffe.com>
2024-04-12 21:54:53 +1200

    start

---
+++ tongariro-cinemas/src/components/CaroselSlide.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>f</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ tongariro-cinemas/src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="movieCarousel">
+    <carousel>
+      <carousel-slide v-for="(slide, index) in slides" :key="slide" :index="index">
+        <img :src="slide" alt="slide" style="width: 100%">
+      </carousel-slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld'
+import Carousel from '@/components/Carousel.vue'
+import CarouselSlide from '@/components/CarouselSlide.vue'
+
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    Carousel,
+    CarouselSlide
+  },
+  data() {
+    return {
+      slides: [
+        'https://picsum.photos/id/237/600/350',
+        'https://picsum.photos/id/236/600/350',
+        'https://picsum.photos/id/235/600/350',
+        'https://picsum.photos/id/234/600/350',
+        'https://picsum.photos/id/233/600/350',
+        'https://picsum.photos/id/232/600/350',
+        'https://picsum.photos/id/231/600/350'
+      ],
+      visibleSlide: 0
+    }
+  }
+}
+</script>
